e153869e3c288af324e6f4f5f1af41721ede6381
Graco2005 <luisgraconeto@gmail.com>
2026-08-07 00:37:44 -0300

    Commit 07/08/26

--- Section3-iniciando_na_programacao_com_python/aula31.py
+++ Section3-iniciando_na_programacao_com_python/aula31.py
@@ -5,15 +5,17 @@ is e is not = é ou não é (tipo, valor, identidade)
 id = identidade
 """
 
-v1 = 'a'
-id_v1 = id(v1)
+condicao = True
+passou_no_if = None
 
-v2 = 'b'
-id_v2 = id(v2)
-
-if id_v1 == id_v2:
-    print('Os ID são iguais.')
-    print('ID 1:', id_v1)
-    print('ID 2:', id_v2)
+if condicao:
+    passou_no_if = True
+    print('Faça algo.')
 else:
-    print('Os IDs são diferentes.')
+    passou_no_if = False
+    print('Não faça algo.')
+
+if passou_no_if is None:
+    print('Não passou no if.')
+else:
+    print('Passou no if.')
---
+++ Section3-iniciando_na_programacao_com_python/aula32.py
@@ -0,0 +1,43 @@
+"""
+Faça um programa que peça ao usuário para digitar um número inteiro,
+informe se este número é par ou ímpar. Caso o usuário não digite um número
+inteiro, informe que não é um número inteiro.
+"""
+# try:
+#     num_int = int(input('Digite um número inteiro: '))
+
+#     if num_int % 2 == 0:
+#         print(f'O número {num_int} é par.')
+#     else:
+#         print(f'O número {num_int} é ímpar.')
+# except:
+#     print('Valor invalido. Digite um número inteiro.')
+"""
+Faça um programa que pergunte a hora ao usuário e, baseando-se no horário 
+descrito, exiba a saudação apropriada. Ex. 
+Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23.
+"""
+# horario = input('Digite o horário atual: ')
+# horario_int = int(horario)
+
+# if 0 <= horario_int <= 11:
+#     print('Bom dia!')
+# elif 12 <= horario_int <= 17:
+#     print('Boa tarde!')
+# elif 18 <= horario_int <= 23:
+#     print('Boa noite!')
+
+"""
+Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letras ou 
+menos escreva "Seu nome é curto"; se tiver entre 5 e 6 letras, escreva 
+"Seu nome é normal"; maior que 6 escreva "Seu nome é muito grande". 
+"""
+# primeiro_nome = input('Digite o seu primeiro nome: ')
+# comprimento_primeiro_nome = len(primeiro_nome)
+
+# if comprimento_primeiro_nome <= 4:
+#     print('Seu nome é curto.')
+# elif 5 <= comprimento_primeiro_nome <= 6:
+#     print('Seu nome é normal.')
+# else:
+#     print('Seu nome é muito grande.')
