d6b44b0636bea75e9ffa639d32644823214d41f3
Graco2005 <luisgraconeto@gmail.com>
2026-08-18 23:35:48 -0300

    Commit 18/08/26

---
+++ Section3-iniciando_na_programacao_com_python/aula60.py
@@ -0,0 +1,14 @@
+"""
+Operação ternária (condicional de uma linha)
+<valor> if <condição> else <outro valor>
+"""
+
+# condicao = 4 == 4
+# variavel = 'Verdade' if condicao else 'Falso'
+# print(variavel)
+
+# digito = 3
+# novo_digito = digito if digito <= 9 else 0
+# print(novo_digito)
+
+print('Valor' if False else 'Outro valor' if False else 'Fim')
---
+++ Section3-iniciando_na_programacao_com_python/aula61.py
@@ -0,0 +1,45 @@
+"""
+Calculo do primeiro dígito do CPF
+CPF: 746.824.890-70
+Colete a soma dos 9 primeiros dígitos do CPF
+multiplicando cada um dos valores por uma
+contagem regressiva começando de 10
+
+Ex.:  746.824.890-70 (746824890)
+   10  9  8  7  6  5  4  3  2
+*  7   4  6  8  2  4  8  9  0
+   70  36 48 56 12 20 32 27 0
+
+Somar todos os resultados: 
+70+36+48+56+12+20+32+27+0 = 301
+Multiplicar o resultado anterior por 10
+301 * 10 = 3010
+Obter o resto da divisão da conta anterior por 11
+3010 % 11 = 7
+Se o resultado anterior for maior que 9:
+    resultado é 0
+contrário disso:
+    resultado é o valor da conta
+
+O primeiro dígito do CPF é 7
+"""
+
+cpf = '746.824.890-70'
+cpf_formatado = cpf.replace('.', '').replace('-', '')[:9]
+
+n = 10
+soma_digitos = 0
+
+for numeros in cpf_formatado:
+    int_numero = int(numeros)
+    soma_digitos += int_numero * n
+    n -= 1
+
+soma_digitos_vezes_10 = soma_digitos * 10
+soma_digitos_vezes_10_resto_divisao = soma_digitos_vezes_10 % 11
+
+resultado = soma_digitos_vezes_10_resto_divisao
+
+resultado = resultado if resultado <= 9 else  0
+
+print(resultado)
